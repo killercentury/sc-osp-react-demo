743402b9190e40373dcb58125fbf954c5173d90b
Kira Cheung <killercentury@gmail.com>
2016-08-09 14:44:29 +1000

    Add new sample for using multiple independent react components

---
+++ src/components/button.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Button extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <a href="#" className="sg-Btn sg-Btn--secondary sg-Btn--large">Search</a>
+    );
+  }
+
+}
+
+export default Button;
